387eb4c6ce76802355482bdc0cb31702c880f21b
germans <grajevskis@gmail.com>
2024-03-24 22:27:09 +0200

    new changes

--- src/pages/MainPage/index.jsx
+++ src/pages/MainPage/index.jsx
@@ -3,13 +3,12 @@ import ProductsOnSaleContainer from '../../components/ProductsOnSaleContainer'
 import c from './index.module.css'
 import CategoriesContainer from '../../components/CategoriesContainer'
 import Footer from '../../components/Footer'
+import s from './index.module.css'
 
 export default function MainPage() {
+
   return (
     <div>
-
-
-      
       <section>
           <h1 className={c.categories_title}>Categories</h1>
             <CategoriesContainer/>
