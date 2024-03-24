66571c1d857774c2c12b0baa78e85ec686f75073
AleksandraBulai <alexandra.bulai2012@gmail.com>
2024-03-24 21:21:26 +0100

    add banner and styles

--- src/pages/MainPage/index.jsx
+++ src/pages/MainPage/index.jsx
@@ -2,17 +2,22 @@ import React from 'react'
 import ProductsOnSaleContainer from '../../components/ProductsOnSaleContainer'
 import c from './index.module.css'
 import CategoriesContainer from '../../components/CategoriesContainer'
+import image_img from './mediaMain/image.png'
 
 export default function MainPage() {
+
+  const banner_styles = {
+    backgroundImage: `url('${image_img}')`
+  }
   return (
-    <div>
-
-
-      
+    <div> 
+      <header>Tamara's Main Page</header>
       <section>
-        <h1 className={c.categories_title}>Categories</h1>
+        <h2 className={c.categories_title}>Categories</h2>
         <CategoriesContainer/>
-        <h1 className={c.sale}>Sale</h1>
+        <div className={c.banner} style={banner_styles}></div>
+        <h1>5% off on the first order</h1>
+        <h2 className={c.sale}>Sale</h2>
             <ProductsOnSaleContainer/>
       </section>
     </div>
