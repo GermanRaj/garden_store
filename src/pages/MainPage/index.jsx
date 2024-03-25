19563defd106020c5e20525a817dbb358fdfe20a
germans <grajevskis@gmail.com>
2024-03-25 15:23:16 +0200

    Add fr function, not all working properly...

--- src/pages/MainPage/index.jsx
+++ src/pages/MainPage/index.jsx
@@ -4,6 +4,7 @@ import c from './index.module.css'
 import CategoriesContainer from '../../components/CategoriesContainer'
 import image_img from './mediaMain/image.png'
 import Footer from '../../components/Footer'
+import s from './index.module.css'
 
 
 export default function MainPage() {
@@ -13,21 +14,34 @@ export default function MainPage() {
   }
   return (
     <div> 
-      <header> Tamara's Main Page </header>
-      <section>
-        <h2 className={c.categories_title}>Categories</h2>
-        <CategoriesContainer/>
-        <div className={c.banner} style={banner_styles}>
-        <h1>5% off on the first order</h1>
-        <p>Name</p>
-        <p>Phone number</p>
-        <p>Email</p>
-        <p>Get a discount</p>
-        </div>
-            <h2 className={c.sale}>Sale </h2>
-            <ProductsOnSaleContainer/>
-        <Footer/>
-      </section>
-    </div>
+        <header> 
+          Tamara's Main Page 
+        </header>
+            <body>
+                  <section>
+                          <section className={s.hrStyle}>
+                            {/* <h2 className={c.categories_title}>Categories</h2>
+                            <hr class='solid'/>
+                            <p>All categories</p> */}
+                          </section>
+                        <CategoriesContainer/>
+                  </section>
+                      <div className={c.banner} style={banner_styles}>
+                            <h1>5% off on the first order</h1>
+                            <p>Name</p>
+                            <p>Phone number</p>
+                            <p>Email</p>
+                            <p>Get a discount</p>
+                      </div>
+                          <section>
+                              <h2 className={c.sale}>Sale </h2>
+                              <ProductsOnSaleContainer/>
+                          </section>
+            </body>
+                          <footer>
+                            <Footer/>
+                          </footer>
+      </div>
+    
   )
 }
