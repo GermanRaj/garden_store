ea163b799caacd3db0f1f21ab004f7edf0a2ace5
germans <grajevskis@gmail.com>
2024-03-27 21:57:39 +0200

    add navigatieon menu, Form , Select and CSS

--- src/pages/AllSalesPage/index.jsx
+++ src/pages/AllSalesPage/index.jsx
@@ -1,7 +1,40 @@
 import React from 'react'
+import s from './index.module.css'
 
 export default function AllSalesPage() {
   return (
-    <div>AllSalesPage</div>
+    <div>
+      <div className={s.navigation}>
+        <p className>Main page</p>
+        <p>__</p>
+        <div></div>
+        <p className={s.navigationAllSales}>All sales</p>
+      </div>
+      <div className={s.title}>
+        <p>Discounted items</p>
+      </div>
+    
+    <div className={s.sortContainer}>
+      
+        <div className={s.priceTitle}>
+          <span>Price </span>
+          <div>
+              <form>
+          <input className={s.formFrom}  type="text" placeholder="from" name="from"/>
+          <input className={s.formTo} type="text" placeholder="to" name="to"/>
+              </form>
+          </div>
+        </div>
+
+
+        <div className={s.sortedTitle}>
+           <span>Sorted </span>
+            <select className={s.byDefault}>
+              <option>by default </option>
+            </select>
+        </div>
+
+    </div>
+    </div>
   )
 }
