e38aa1134ccfa952d3c8775f687dade43d53a42e
germans <grajevskis@gmail.com>
2024-03-30 16:30:01 +0200

    Add Footer to App.js -> new Route, add new Function in ProductsReducer, Create visible Cards and made css

--- src/pages/AllSalesPage/index.jsx
+++ src/pages/AllSalesPage/index.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
 import s from './index.module.css'
+import AllProductsOnSalesContainer from '../../components/AllProductsOnSalesContainer'
+import {  useDispatch, useSelector } from 'react-redux';
+import { sortProductsOnSalesAction } from '../../store/reducers/productsOnSaleReducer'
+
+
 
 export default function AllSalesPage() {
+  
+  const dispatch = useDispatch();
+
+  const productsBySaleState = useSelector(store =>store.productsOnSales )
+
+  const order = event => {
+  dispatch(sortProductsOnSalesAction(event.target.value))
+}
+
   return (
     <div>
       <div className={s.navigation}>
@@ -15,7 +28,6 @@ export default function AllSalesPage() {
       </div>
     
     <div className={s.sortContainer}>
-      
         <div className={s.priceTitle}>
           <span>Price </span>
           <div>
@@ -24,17 +36,21 @@ export default function AllSalesPage() {
           <input className={s.formTo} type="text" placeholder="to" name="to"/>
               </form>
           </div>
-        </div>
-
+    </div>
 
         <div className={s.sortedTitle}>
            <span>Sorted </span>
-            <select className={s.byDefault}>
-              <option>by default </option>
+            <select onInput={order} className={s.byDefault}>
+                <option value='by_default'>By default</option>
+                <option value='name'>By name (A-Z)</option>
+                <option value='price_asc'>By price (ASC)</option>
+                <option value='price_desc'>By price (DESC)</option>
             </select>
         </div>
-
     </div>
-    </div>
+    <AllProductsOnSalesContainer productsBySaleState ={productsBySaleState}/>    
+  </div>
+  
   )
+  
 }
