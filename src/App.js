7809af0989dcf797f39d78f12faf050ebdbefd78
Andrusenko.Artem <andrusenkoartem2005@gmail.com>
2024-04-01 15:30:35 +0200

    fix css

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ function App() {
         <Route path='/allproducts' element={<AllProductPage/>}></Route>
         <Route path='/allsales' element={<AllSalesPage/>}></Route>
       </Routes>
+      <Footer/>
     </div>
   );
 }
