9c71c1e60aa3ad495ed116eb2e67ab8b78111091
germans <grajevskis@gmail.com>
2024-04-05 20:20:06 +0300

    Add SingleProductPage css+figma and form

--- src/components/SingeProductCard/index.jsx
+++ src/components/SingeProductCard/index.jsx
@@ -1,13 +1,34 @@
 import React from 'react'
 import { domen } from '../../domen'
+import s from './index.module.css'
 
-export default function SingleProductCard({title, image}) {
+export default function SingleProductCard({title, image, discont_price , price, description }) {
 
     const img = domen+image;
   return (
-    <div>
-    <img src={img} alt={title} />
-      <p>{title}</p>
+    <div className={s.productCard}>
+            <img src={img} alt={title} />
+                <div className={s.textPosition}>
+                    <h3> {title} </h3>
+                    <section className={s.PricesAndDiscounts}>
+                        <p> ${discont_price}</p> 
+                        <p> ${price} </p> 
+                        <p>-{Math.floor(100-(discont_price*100/price))} %</p>
+                    </section>
+                            <section className={s.cartFunction}>
+                                <div className={s.addOrDeleteForm}>
+                                    <p>-</p>
+                                    <p>1</p>
+                                    <p>+</p>
+                               </div>
+                                  <button>Add to cart</button>
+                            </section>
+                                    <section className={s.blockfooter}>
+                                        <h3>Description</h3>
+                                        <p className={s.description}>{description}</p>
+                                        <p>Read more</p>
+                                    </section>
+                </div>
     </div>
   )
 }
