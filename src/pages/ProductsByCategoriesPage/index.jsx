cce0c68ee932689ea53481fdff91ed473095bafe
Maryam <tamarahaak@web.de>
2024-04-09 22:16:35 +0200

    categorytitleremove

--- src/pages/ProductsByCategoriesPage/index.jsx
+++ src/pages/ProductsByCategoriesPage/index.jsx
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom';
 import React, { useEffect,} from 'react';
 import {  useDispatch, useSelector } from 'react-redux';
 import { getProductsByCategorie } from '../../Requests/productsByCategories';
+import ProductsByCategorieContainer from '../../components/ProductsByCategorieContainer';
 
 
 
