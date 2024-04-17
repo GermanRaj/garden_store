2f25385dec1d9a161d47839197d8e457c0c5bb5f
germans <grajevskis@gmail.com>
2024-04-17 21:56:55 +0300

    add ModalWindow

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,17 +1,16 @@
 import React from 'react'
 import s from './index.module.css'
+import closeSign from './media/delete.png'
 
-export default function Modal({active, setActive}) {
+
+export default function Modal({closeModalWindow, modalActive}) {
     return (
-        <div
-          className={active ? "modal_active" : "modal"}
-          onClick={() => setActive(true)} 
-        >
-          <section className={s.modal_content} onClick={(e) => e.stopPropagation()}>
+        <div className={[s.modal, modalActive ? s.active : ''].join(' ')}>
+          <section className={s.modal_content} >
             <p>Congratulations!</p>
             <p>Your order has been successfully placed on the website.</p>
             <p>A manager will contact you shortly to confirm your order.</p>
-            <div>X</div>
+            <img onClick={closeModalWindow} src={closeSign} alt="closeButton" />
           </section>
         </div>
       );
