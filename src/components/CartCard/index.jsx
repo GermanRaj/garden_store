5af23ee62d118908add57a59abfab1b4d152da85
AleksandraBulai <alexandra.bulai2012@gmail.com>
2024-04-21 02:17:34 +0200

    add styles css in CartPage, CartCart, fixed problems with counts

--- src/components/CartCard/index.jsx
+++ src/components/CartCard/index.jsx
@@ -13,17 +13,16 @@ export default function CartCard({id, image, title, price, discont_price, count}
                 <img src={`${domen}${image}`} alt={title} />
                 <h6>{title}</h6>
                 <p>{discont_price}</p>
-                <button onClick={() => dispatch(decrCountAction(id))}
-                >-</button>
-                 <p>{count}</p>
-                <button onClick={() =>dispatch(incrCountAction(id))}
-                >+</button>
+                <div className={s.buttons} onClick={() => dispatch(decrCountAction(id))}
+                >-</div>
+                 <p className={s.quantity}>{count}</p>
+                <div className={s.buttons} onClick={() =>dispatch(incrCountAction(id))}
+                >+</div>
                 <h4>${price * count}</h4>
                 <h6 onClick={()=>dispatch(deleteCartItemAction(id))}
                 >X</h6>
           </div>
       )
     }
-
 
 
