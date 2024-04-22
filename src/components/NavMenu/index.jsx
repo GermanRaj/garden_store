496a97b7e6e9ff736758348478a812c5085782ed
AleksandraBulai <alexandra.bulai2012@gmail.com>
2024-04-22 19:45:26 +0200

    fix cart icon + total sum

--- src/components/NavMenu/index.jsx
+++ src/components/NavMenu/index.jsx
@@ -29,13 +29,12 @@ export default function NavMenu() {
       <div className={s.logo_img}> 
         <img src={logo} alt='logo'/>
         </div>
-     
+    
     <div className={s.nav_menu}>
-     <Link to='/'>Main Page</Link>
-     <Link to='/categories'>Categories</Link>
-     <Link to='/allproducts'>All products</Link>
-     <Link to='/allsales'> All sales </Link>
-    </div>
+    <Link to='/'>Main Page</Link>
+    <Link to='/categories'>Categories</Link>
+    <Link to='/allproducts'>All products</Link>
+    <Link to='/allsales'> All sales </Link></div>
     <Link to={'/cart'}><div className={s.basket} style={basket_styles}></div></Link>
     <Link to={'/'}><IoMenu className={s.menu_icon} /></Link>
     <p className={s.count}>{totalCount}</p>
